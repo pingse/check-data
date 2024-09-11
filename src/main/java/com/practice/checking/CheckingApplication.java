a0191ea2c54858ec31c2b91da25bfdc501a83126
Pin <ppyj48625@naver.com>
2024-09-11 14:51:21 +0900

    feature: Init spring boot

---
+++ src/main/java/com/practice/checking/CheckingApplication.java
@@ -0,0 +1,13 @@
+package com.practice.checking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckingApplication.class, args);
+	}
+
+}
